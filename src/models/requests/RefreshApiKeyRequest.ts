8986cbe186680129ab128c68ea7d2e27bd670152
Juan Pablo Rodriguez Sotto <juan.rodriguezsotto@gmail.com>
2022-09-24 17:13:35 -0300

    Remove unused request schema

--- src/models/requests/RefreshApiKeyRequest.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const RefreshApiKeyRequestSchema = z.object({
-  params: z.object({
-    familyId: z.number(),
-  }),
-});
-
-export type RefreshApiKeyRequest = z.infer<typeof RefreshApiKeyRequestSchema>;
