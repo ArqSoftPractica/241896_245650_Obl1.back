066ad699b7db02ae51d1fb2455207fcff4b7ac9c
Juan Pablo Rodriguez Sotto <juan.rodriguezsotto@gmail.com>
2022-09-26 01:26:41 -0300

    Fix request validation

--- src/models/requests/GetExpensesRequest.ts
+++ src/models/requests/GetExpensesRequest.ts
@@ -4,13 +4,13 @@ export const GetExpensesRequestSchema = z.object({
   query: z.object({
     from: z
       .preprocess((arg) => {
-        if (typeof arg == 'string') return parseInt(arg);
-      }, z.number())
+        if (typeof arg == 'string' || arg instanceof Date) return new Date(arg);
+      }, z.date())
       .optional(),
     to: z
       .preprocess((arg) => {
-        if (typeof arg == 'string') return parseInt(arg);
-      }, z.number())
+        if (typeof arg == 'string' || arg instanceof Date) return new Date(arg);
+      }, z.date())
       .optional(),
     skip: z
       .preprocess((arg) => {
