2526da9fbd08f15160bf55a76d599f1b7b217c9b
fernandasecinaro <secinarof@gmail.com>
2022-10-04 23:07:41 -0300

    tests not working

---
+++ src/tests/controllerTests/users.test.ts
@@ -0,0 +1,27 @@
+
+describe('Register new administrator', () => {
+  test('Register new admin successfully', () => {
+    // const req: Request = {
+    //   body: {
+    //     email: '',
+    //     password: '123456',
+    //     name: 'John',
+    //     familyName: 'Doe',
+    //   },
+    // } as Request;
+    // const res: Response = {
+    //   status: jest.fn().mockReturnThis(),
+    //   json: jest.fn().mockReturnThis(),
+    // } as unknown as Response;
+    // const mock = jest.fn<IUsersService, []>(() => ({
+    //   registerAdmin: jest.fn().mockReturnValue(Promise.resolve()),
+    //   registerUser: jest.fn().mockReturnValue(Promise.resolve()),
+    //   inviteUser: jest.fn().mockReturnValue(Promise.resolve()),
+    // }));
+    // const usersService = new mock();
+    // const usersController = new UsersController(usersService);
+    // usersController.registerAdmin(req, res);
+    // expect(res.status).toHaveBeenCalledWith(201);
+    expect(true).toBeTruthy();
+  });
+});
---
+++ src/tests/serviceTests/users.test.ts
@@ -0,0 +1,85 @@
+import { RegisterAdminRequest } from 'models/requests/RegisterAdminRequest';
+import { IUsersRepository } from 'repositoryTypes/IUsersRepository';
+import UsersService from 'services/UsersService';
+import IAuthService from 'serviceTypes/IAuthService';
+import { IEmailService } from 'serviceTypes/IEmailService';
+
+// describe('Register new administrator', () => {
+//   test('Register new admin successfully', () => {
+//     const req: RegisterAdminRequest = {
+//       body: {
+//         email: 'secinarof@gmail.com',
+//         password: '123456',
+//         name: 'Fernanda',
+//         familyName: 'Secinaro',
+//       },
+//     } as RegisterAdminRequest;
+
+//     const mock = jest.fn<IUsersRepository, []>(() => ({
+//       createUser: jest.fn().mockReturnValue(Promise.resolve()),
+//       getUserByEmail: jest.fn().mockReturnValue(Promise.resolve(null)),
+//     }));
+
+//     const mockAuth = jest.fn<IAuthService, []>(() => ({
+//       createInviteToken: jest.fn().mockReturnValue(Promise.resolve()),
+//       verifyInviteToken: jest.fn().mockReturnValue(Promise.resolve()),
+//       createToken: jest.fn().mockReturnValue(Promise.resolve()),
+//       verifyToken: jest.fn().mockReturnValue(Promise.resolve()),
+//       login: jest.fn().mockReturnValue(Promise.resolve()),
+//       refreshApiKey: jest.fn().mockReturnValue(Promise.resolve()),
+//       getApiKey: jest.fn().mockReturnValue(Promise.resolve()),
+//     }));
+
+//     const mockEmail = jest.fn<IEmailService, []>(() => ({
+//       sendInviteEmail: jest.fn().mockReturnValue(Promise.resolve()),
+//     }));
+
+//     const usersRepository = new mock();
+//     const authService = new mockAuth();
+//     const emailService = new mockEmail();
+//     const usersController = new UsersService(usersRepository, authService, emailService);
+
+//     usersController.registerAdmin(req);
+//   });
+// });
+
+describe('Register new administrator', () => {
+  // expect(() => {
+  test('Register admin with existent email', () => {
+    const req: RegisterAdminRequest = {
+      body: {
+        email: 'secinarof@gmail.com',
+        password: '123456',
+        name: 'Fernanda',
+        familyName: 'Secinaro',
+      },
+    } as RegisterAdminRequest;
+
+    const mock = jest.fn<IUsersRepository, []>(() => ({
+      createUser: jest.fn().mockReturnValue(Promise.resolve()),
+      getUserByEmail: jest.fn().mockReturnValue(Promise.resolve(42)),
+    }));
+
+    const mockAuth = jest.fn<IAuthService, []>(() => ({
+      createInviteToken: jest.fn().mockReturnValue(Promise.resolve()),
+      verifyInviteToken: jest.fn().mockReturnValue(Promise.resolve()),
+      createToken: jest.fn().mockReturnValue(Promise.resolve()),
+      verifyToken: jest.fn().mockReturnValue(Promise.resolve()),
+      login: jest.fn().mockReturnValue(Promise.resolve()),
+      refreshApiKey: jest.fn().mockReturnValue(Promise.resolve()),
+      getApiKey: jest.fn().mockReturnValue(Promise.resolve()),
+    }));
+
+    const mockEmail = jest.fn<IEmailService, []>(() => ({
+      sendInviteEmail: jest.fn().mockReturnValue(Promise.resolve()),
+    }));
+
+    const usersRepository = new mock();
+    const authService = new mockAuth();
+    const emailService = new mockEmail();
+    const usersController = new UsersService(usersRepository, authService, emailService);
+
+    usersController.registerAdmin(req);
+  });
+  // }).toThrow(InvalidDataError);
+});
