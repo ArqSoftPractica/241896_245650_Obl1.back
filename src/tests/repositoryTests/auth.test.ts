9a4f0efba2bbac9540d572a3f720ca79a6f082fe
Juan Pablo Rodriguez Sotto <juan.rodriguezsotto@gmail.com>
2022-10-06 01:10:39 -0300

    Fix repository tests

--- src/tests/repositoryTests/auth.test.ts
+++ src/tests/repositoryTests/auth.test.ts
@@ -1,72 +1,13 @@
-// afterAll(async () => {
-//   const deleteUsers = prisma.user.deleteMany();
-//   const deleteFamilies = prisma.family.deleteMany();
-//   const deleteCategories = prisma.category.deleteMany();
+import prisma from 'models/client';
 
-//   await prisma.$transaction([deleteUsers, deleteFamilies, deleteCategories]);
-//   await prisma.$disconnect();
-// });
+afterEach(async () => {
+  await prisma.user.deleteMany();
+  await prisma.category.deleteMany();
+  await prisma.family.deleteMany();
 
-it('create new administrator', async () => {
-  //   const user = {
-  //     name: 'Hermione Granger',
-  //     email: 'hermione@hogwarts.io',
-  //     password: '123456',
-  //     role: Role.admin,
-  //     family: {
-  //       connectOrCreate: {
-  //         where: { name: 'Secinaro' },
-  //         create: { name: 'Secinaro', apiKey: `family-costs-1` },
-  //       },
-  //     },
-  //   };
-
-  //   const usersRepo = new UsersRepository();
-  //   await usersRepo.createUser(user);
-
-  //   const newUser = await prisma.user.findUnique({
-  //     where: {
-  //       email: user.email,
-  //     },
-  //   });
-
-  //   const newFamily = await prisma.family.findUnique({
-  //     where: {
-  //       name: user.family.connectOrCreate.create.name,
-  //     },
-  expect(true).toBe(true);
+  await prisma.$disconnect();
 });
 
-//   expect(newUser?.email).toEqual(user.email);
-//   expect(newUser?.name).toEqual(user.name);
-//   expect(newUser?.password).toEqual(user.password);
-//   expect(newUser?.role).toEqual(user.role);
-//   expect(newUser?.familyId).toEqual(newFamily?.id);
-// });
-
-// it('Get User by email', async () => {
-//   const user = {
-//     name: 'Hermione Granger',
-//     email: 'hermione@hogwarts.io',
-//     password: '123456',
-//     role: Role.admin,
-//     family: {
-//       connectOrCreate: {
-//         where: { name: 'Secinaro' },
-//         create: { name: 'Secinaro', apiKey: `family-costs-1` },
-//       },
-//     },
-//   };
-
-//   const newUser = await prisma.user.create({
-//     data: user,
-//   });
-
-//   const usersRepo = new UsersRepository();
-//   const userFetched = await usersRepo.getUserByEmail(user.email);
-
-//   expect(newUser?.email).toEqual(user.email);
-//   expect(newUser?.name).toEqual(user.name);
-//   expect(newUser?.password).toEqual(user.password);
-//   expect(newUser?.role).toEqual(user.role);
-// });
+it('create new administrator', async () => {
+  expect(true).toBe(true);
+});
--- src/tests/repositoryTests/users.test.ts
+++ src/tests/repositoryTests/users.test.ts
@@ -1,72 +1,73 @@
-// afterAll(async () => {
-//   const deleteUsers = prisma.user.deleteMany();
-//   const deleteFamilies = prisma.family.deleteMany();
-//   const deleteCategories = prisma.category.deleteMany();
+import { Role } from '@prisma/client';
+import prisma from 'models/client';
+import UsersRepository from 'repository/usersRepository';
 
-//   await prisma.$transaction([deleteUsers, deleteFamilies, deleteCategories]);
-//   await prisma.$disconnect();
-// });
-
-it('create new administrator', async () => {
-  //   const user = {
-  //     name: 'Hermione Granger',
-  //     email: 'hermione@hogwarts.io',
-  //     password: '123456',
-  //     role: Role.admin,
-  //     family: {
-  //       connectOrCreate: {
-  //         where: { name: 'Secinaro' },
-  //         create: { name: 'Secinaro', apiKey: `family-costs-1` },
-  //       },
-  //     },
-  //   };
-
-  //   const usersRepo = new UsersRepository();
-  //   await usersRepo.createUser(user);
-
-  //   const newUser = await prisma.user.findUnique({
-  //     where: {
-  //       email: user.email,
-  //     },
-  //   });
-
-  //   const newFamily = await prisma.family.findUnique({
-  //     where: {
-  //       name: user.family.connectOrCreate.create.name,
-  //     },
-  //   });
-
-  //   expect(newUser?.email).toEqual(user.email);
-  //   expect(newUser?.name).toEqual(user.name);
-  //   expect(newUser?.password).toEqual(user.password);
-  //   expect(newUser?.role).toEqual(user.role);
-  //   expect(newUser?.familyId).toEqual(newFamily?.id);
-  expect(true).toBe(true);
+afterEach(async () => {
+  await prisma.user.deleteMany();
+  await prisma.category.deleteMany();
+  await prisma.family.deleteMany();
+  await prisma.$disconnect();
 });
 
-// it('Get User by email', async () => {
-//   const user = {
-//     name: 'Hermione Granger',
-//     email: 'hermione@hogwarts.io',
-//     password: '123456',
-//     role: Role.admin,
-//     family: {
-//       connectOrCreate: {
-//         where: { name: 'Secinaro' },
-//         create: { name: 'Secinaro', apiKey: `family-costs-1` },
-//       },
-//     },
-//   };
+it('create new administrator', async () => {
+  const user = {
+    name: 'Hermione Granger',
+    email: 'hermione@hogwarts.io',
+    password: '123456',
+    role: Role.admin,
+    family: {
+      connectOrCreate: {
+        where: { name: 'Secinaro' },
+        create: { name: 'Secinaro', apiKey: `family-costs-1` },
+      },
+    },
+  };
 
-//   const newUser = await prisma.user.create({
-//     data: user,
-//   });
+  const usersRepo = new UsersRepository();
+  await usersRepo.createUser(user);
 
-//   const usersRepo = new UsersRepository();
-//   const userFetched = await usersRepo.getUserByEmail(user.email);
+  const newUser = await prisma.user.findUnique({
+    where: {
+      email: user.email,
+    },
+  });
 
-//   expect(newUser?.email).toEqual(user.email);
-//   expect(newUser?.name).toEqual(user.name);
-//   expect(newUser?.password).toEqual(user.password);
-//   expect(newUser?.role).toEqual(user.role);
-// });
+  const newFamily = await prisma.family.findUnique({
+    where: {
+      name: user.family.connectOrCreate.create.name,
+    },
+  });
+
+  expect(newUser?.email).toEqual(user.email);
+  expect(newUser?.name).toEqual(user.name);
+  expect(newUser?.password).toEqual(user.password);
+  expect(newUser?.role).toEqual(user.role);
+  expect(newUser?.familyId).toEqual(newFamily?.id);
+});
+
+it('Get User by email', async () => {
+  const user = {
+    name: 'Hermione Granger',
+    email: 'hermione@hogwarts.io',
+    password: '123456',
+    role: Role.admin,
+    family: {
+      connectOrCreate: {
+        where: { name: 'Secinaro' },
+        create: { name: 'Secinaro', apiKey: `family-costs-1` },
+      },
+    },
+  };
+
+  const newUser = await prisma.user.create({
+    data: user,
+  });
+
+  const usersRepo = new UsersRepository();
+  const userFetched = await usersRepo.getUserByEmail(user.email);
+
+  expect(newUser?.email).toEqual(user.email);
+  expect(newUser?.name).toEqual(user.name);
+  expect(newUser?.password).toEqual(user.password);
+  expect(newUser?.role).toEqual(user.role);
+});
