041e520e2df577019da778e8ad0e5550d5746497
Juan <juan.rodriguezsotto@gmail.com>
2022-11-22 23:43:53 -0300

    Add sqs support for queuing messages

---
+++ src/services/MQEmailService.ts
@@ -0,0 +1,53 @@
+import { injectable } from 'inversify';
+import 'reflect-metadata';
+
+import { IEmailService } from 'serviceTypes/IEmailService';
+import { SendMessageCommand } from '@aws-sdk/client-sqs';
+import { sqsClient } from 'helpers/sqsClient';
+
+@injectable()
+class MQEmailService implements IEmailService {
+  private readonly FRONTEND_URL = process.env.FRONTEND_APP_URL ?? 'http://localhost:3000';
+  private readonly SQS_QUEUE_URL = process.env.SQS_QUEUE_URL ?? 'http://localhost:4566/000000000000/test';
+
+  private getInviteLink(token: string): string {
+    return `${this.FRONTEND_URL}/register?token=${token}`;
+  }
+
+  private getInviteEmailBody(link: string): string {
+    return `You have been invited to join the family. Please click on the link below to register and join the family. \n\n${link}`;
+  }
+
+  public async sendInviteEmail(email: string, token: string): Promise<void> {
+    const link = this.getInviteLink(token);
+    const body = this.getInviteEmailBody(link);
+
+    const params = {
+      DelaySeconds: 10,
+      MessageAttributes: {
+        Receiver: {
+          DataType: 'String',
+          StringValue: email,
+        },
+        Subject: {
+          DataType: 'String',
+          StringValue: 'Family Invite',
+        },
+        Body: {
+          DataType: 'String',
+          StringValue: body,
+        },
+      },
+      MessageBody: JSON.stringify({
+        receiver: email,
+        subject: 'Family Invite',
+        body: body,
+      }),
+      QueueUrl: this.SQS_QUEUE_URL,
+    };
+    const data = await sqsClient.send(new SendMessageCommand(params));
+    console.log('Success, message sent. MessageID:', data.MessageId);
+  }
+}
+
+export default MQEmailService;
